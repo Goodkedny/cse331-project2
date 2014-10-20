0bae1a4c88856cc42d18e10667a30eb536b09ecc
Izzy Cecil <lorr.cecil@gmail.com>
2014-10-20 16:21:56 -0600

    Added implementation for is_hit and helping methods.

--- include/cache.h
+++ include/cache.h
@@ -1,6 +1,13 @@
 #pragma once
 
-typedef unsigned long Cache;
+#include <stdbool.h>
+
+typedef unsigned int Index;
+typedef unsigned int Tag;
+typedef struct {
+  Tag tag;
+  bool valid;
+}Cache;
 
 typedef struct {
   unsigned long load_hit;
@@ -22,9 +29,42 @@ typedef struct {
   unsigned offset_bits;
 } CacheConf;
 
-int is_hit();
-int is_hit_direct();
-int is_hit_associative();
+/**
+ * `is_hit` will, given a cache and its associativity, look up a given index-tag
+ * pair. If it is found in the cache, it will return true. Else it will return
+ * false.
+ *
+ * @param cache Array of cache slots to be queried.
+ * @param direct_mapped Flaggs whether this is a direct or associative lookup.
+ * @param slots Number of slots in the cache.
+ * @param tag Tag used to check if the content in a slot is the desired content.
+ * @param index The direct index to use if cache is mapped.
+ *
+ * @return If the memory was in cache or not.
+ */
+bool is_hit(Cache* cache, bool direct_mapped, int slots, Tag tag, Index index);
+
+/**
+ * Like `is_hit` but used for direct-mapped cache. Called by `is_hit`.
+ *
+ * @param cache Array of cache slots to be queried.
+ * @param tag Tag used to check if the content in a slot is the desired content.
+ * @param index The direct index to lookup.
+ *
+ * @return If the memory was in cache or not.
+ */
+bool is_hit_direct(Cache* cache, Tag tag, Index index);
+
+/**
+ * Like `is_hit` but used for fully associative cache. Called by `is_hit`.
+ *
+ * @param cache Array of cache slots to be queried.
+ * @param slots The length of cache.
+ * @param tag Tag used to check if the content in a slot is the desired content.
+ *
+ * @return If the memory was in cache or not.
+ */
+bool is_hit_associative(Cache* cache, int slots, Tag tag);
 
 SimResult sim_load();
 int on_load_miss();
@@ -34,5 +74,5 @@ int load_FIFO_replacement();
 SimResult sim_store();
 int on_store_miss();
 int store_no_write_allocate();
-// Call on_load_miss 
+// Call on_load_miss
 int store_write_allocate();
